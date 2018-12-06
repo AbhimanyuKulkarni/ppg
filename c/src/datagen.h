c329c85e32dceca175ad925611e1cd9d21dfd37e
shantanu <shantanu.gupta33@gmail.com>
2018-12-05 20:29:52 -0600

    More data generation functions from original samples to compressed; to use for when files are not available. Also includes PhysioNet data dump.

---
+++ c/src/datagen.h
@@ -0,0 +1,16 @@
+#ifndef PPG_DATAGEN_H
+#define PPG_DATAGEN_H
+#include "setup.h"
+// Generate a random sampling matrix
+void get_random_sample_flags(size_t N, size_t m, bool *flags);
+
+// Use sampling flags to get compressed samples.
+void get_compressed_samples(double *samples, PPG_Params params,
+														bool *phi_flags, double *compressed);
+
+// Generate simulated cosine wave data from params, into samples.
+void generate_sim_data(PPG_Params params, double *samples);
+
+// Load PhysioNet data into samples, along with params.
+void get_physionet_data(PPG_Params *params, double *samples);
+#endif
